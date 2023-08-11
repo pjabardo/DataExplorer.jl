7d1b981a3e67271e1e21bebd1b3891d2cb0a1253
= <pjabardo@ipt.br>
2023-08-11 16:10:47 -0300

    Worked on calculating damping ratio
    But I believe I will change much here...

--- src/damping.jl
+++ src/damping.jl
@@ -1,4 +1,4 @@
-export logdecrement
+export logdecrement, vibration_damping
 
 import Polynomials: fit, Polynomial, coeffs
 
@@ -12,7 +12,7 @@ function logdecrement(x::AbstractVector{T}) where {T}
 
     istart = findfirst(>(idx_max), idx)
     nw = 1
-    ampl = Point2{T}[]
+    ampl = T[]
     for i in eachindex(idx[istart:end-1])
         k = idx[i]
         kn = idx[i+1]
@@ -26,13 +26,52 @@ function logdecrement(x::AbstractVector{T}) where {T}
         a₁, a₂, a₃ = coeffs(p)
         tmax = -a₂/2a₃
         xmax = p(tmax)
-        println(imax, "  ", tmax)
-        push!(ampl, Point2{T}(tmax, xmax))
+        push!(ampl, xmax)
     end
-
     return ampl
 
 end
 
     
-#function dampingratio(
+function vibration_damping(x::AbstractVector{T}) where {T}
+    x = detrend(x)
+    y = abs.(x)
+    idx_max = argmax(y)
+
+    idx = levelcrossings_index(x, 0, bothdirs=true)
+
+    N = size(x,1)
+    
+    t₀ = Float64[]
+    
+    istart = findfirst(>(idx_max), idx)
+    nw = 1
+    ampl = T[]
+    for i in eachindex(idx[istart:end-1])
+        
+        k = idx[i]
+        kn = idx[i+1]
+
+        push!(t₀, linear_interp((k, k+1), (x[k], x[k+1]), 0))
+        
+        
+        yi = y[k:kn]
+        imax = argmax(yi)-1 + k
+        limlft = max(imax-nw, firstindex(y))
+        limrgt = min(imax+nw, lastindex(y))
+        ii = limlft:limrgt
+        yfit= y[ii]
+        p = fit(Polynomial, ii, yfit, 2)
+        a₁, a₂, a₃ = coeffs(p)
+        tmax = -a₂/2a₃
+        xmax = p(tmax)
+        push!(ampl, xmax)
+    end
+    i = idx[end]
+    
+    push!(t₀, linear_interp( (i-1, i), (x[i-1], x[i]), 0))
+    
+    return ampl, t₀
+
+end
+
--- src/tseries.jl
+++ src/tseries.jl
@@ -1,4 +1,4 @@
-export TSeries, samplerate, starttime, sampletimes
+export TSeries, samplerate, nframes, starttime, sampletimes
 
 struct TSeries{N,X,XA<:AbstractArray{X,N},T} <: AbstractArray{X,N}
     fs::T
@@ -15,7 +15,7 @@ Base.size(x::TSeries,idx...) = size(x.x,idx...)
 
 samplerate(x::TSeries) = x.fs
 starttime(x::TSeries) = x.t0
-nsamples(x::TSeries) = size(x.x,1)
+nframes(x::TSeries) = size(x.x,1)
 
 sampletimes(x::TSeries) = range(starttime(x), step=1/samplerate(x), length=nsamples(x))
 
